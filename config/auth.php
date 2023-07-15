68f11ec2483c7a7f2785bc2c6cfb74e074cffd82
Location <jamesabcde277@gmail.com>
2023-07-15 10:02:09 +0800

    add user controller/user database table

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -36,9 +36,10 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'members',
+            'hash' => false,
         ],
     ],
 
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('user', 'UserController@store'); //註冊
+Route::post('login', 'LoginController@login'); //登入
+
+
+
+
+Route::middleware('auth:api')->get('user', 'UserController@index');  //查看
+Route::middleware('auth:api')->put('user', 'UserController@update'); //編輯
+Route::middleware('auth:api')->delete('user/{members}', 'UserController@destroy'); //刪除
+Route::middleware('auth:api')->get('logout', 'LogoutController@logout'); //登出
