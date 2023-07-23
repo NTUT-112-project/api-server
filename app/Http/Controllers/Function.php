015f520f573df7af72c2d518fa6018f49c85ca8e
Ray87654 <ray87654@gmail.com>
2023-07-23 20:58:37 +0800

    Testing for API

---
+++ app/Http/Controllers/Function.php
@@ -0,0 +1,33 @@
+<?php
+// Use Folloing code in terminal at app folder
+// composer require openai-php/client
+// composer require php-http/discovery php-http/async-client-implementation:*
+
+require_once('app//vendor//autoload.php');
+// For loading OpenAI libary
+
+require_once('key.php');
+$api_key = GPTAPI_key;
+// For safety reason
+
+$client = OpenAI::client($api_key);
+
+$question_to_GPT = "將下列文本翻譯至中文:";
+
+$question = "The magnitude of the universe is unimaginable.
+And our world is just a cloud of dust compared to universe .";
+
+$prompt = $question_to_GPT . $question;
+
+$result = $client->chat()->create([
+    'model' => 'gpt-3.5-turbo',
+    'messages' => [
+        ['role' => 'user', 'content' => $prompt]
+    ],
+]);
+// Use "text-davinci-003" need to set "max_token"
+
+echo $result['choices'][0]['message']['content']; 
+// Response only 4096 token (16384 characters)
+
+?>
---
+++ app/Http/Controllers/key.php
@@ -0,0 +1,4 @@
+<?php
+define('GPTAPI_key', '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');
+// https://platform.openai.com/account/api-keys for setting api key
+?>
