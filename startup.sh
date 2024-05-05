b9cde311b78e38f53deb83fdd754f803aa555b44
James-Lu-none <jamesabcde277@gmail.com>
2024-05-05 19:10:13 +0800

    chore: Update startup.sh to include docker compose down command

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@
 # Change to the directory where your docker-compose.yml file is located
 cd "$(dirname "$0")"
 
+docker compose down
 # Run docker-compose up command
 docker compose --env-file ./src/.env up --build -d
