4b6a85fe96969a596a9cc9563b5952cedfe599f3
James-Lu-none <jamesabcde277@gmail.com>
2024-09-12 15:50:11 +0800

    move commands to entrypoint avoid volume mount overriding

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# Install important docker dependencies
+composer install
+
+# set permissions
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan key:generate
+php artisan migrate
+php artisan clear-compiled
+composer dump-autoload
+php artisan optimize
+
 exec php-fpm
