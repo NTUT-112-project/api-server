ce0ea350c830fe758009cf30fe02cf50325a065c
James-Lu-none <jamesabcde277@gmail.com>
2024-09-13 00:48:03 +0800

    add model demo on /hi endpoint

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,5 +17,21 @@ Route::get('/', function () {
 });
 
 Route::get('/hi',function(){
-    return "hi";
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, "http://ollama:11434/api/generate");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+        "model" => "gemma2:27b",
+        "prompt" => "Hi"
+    ]));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json'
+    ]);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
 });
